1b7d347cd0b2fcd918ac40fd534fc2f5c25fee34
Estevao Hilst <xtevan@gmail.com>
2018-07-27 13:39:37 -0300

    Added project skeleton, .gitignore and README.md files

---
+++ src/main/java/com/hilst/totalrecipeservice/TotalRecipeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hilst.totalrecipeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalRecipeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalRecipeServiceApplication.class, args);
+	}
+}
